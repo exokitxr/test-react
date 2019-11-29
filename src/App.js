54bca2c6f50348ff0c8ce8fb5b005335f3ca7431
Chris <chris.eddy.dev@gmail.com>
2019-11-29 00:10:21 -0800

    Bring avatars into react, parse html and run scripts

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+// import './App.css';
+import Navbar from './Navbar';
+import WebXRApp from './WebXRApp';
+import AvatarsApp from './WebXRApps/avatars-master/index.html';
+// import BrowserApp from './WebXRApps/exokit-browser-master/index.html';
+
+import { parse } from 'node-html-parser';
+
+
+const App = props => {
+
+  useEffect(() => {
+    const parsedHTML = parse(AvatarsApp);
+    const scripts = parsedHTML.childNodes[0].querySelectorAll('script');
+    console.log("WebXRApp: ", scripts)
+
+    scripts.forEach(s => {
+      console.log("loading:", s.rawAttrs)
+      const script = document.createElement("script");
+      if(s.attributes && s.attributes.src){
+        script.src = `/WebXRApps/avatars-master/${s.attributes && s.attributes.src ? s.attributes.src : ""}`;
+        script.async = false;
+        document.body.appendChild(script);
+      }
+      else{
+        script.src = `/WebXRApps/avatars-master/${s.attributes && s.attributes.src ? s.attributes.src : ""}`;
+        script.async = false;
+        document.body.appendChild(script);
+
+      }
+  })
+})
+
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <WebXRApp html={{__html: AvatarsApp}} />
+      {/* <WebXRApp html={{__html: BrowserApp}} /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WebXRApp.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const WebXRApp = props => {
+   return <div dangerouslySetInnerHTML={props.html} />
+}
+export default WebXRApp;
